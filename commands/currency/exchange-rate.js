cf5724946c6c1c5fdec9a5e31872c4e9a335a71d
chromeosenjoyer <134458207+chromeosenjoyer@users.noreply.github.com>
2023-05-24 21:58:58 -0300

    Added exchange-rate command
    - Created a command to fetch the exchange rate from one currency to another.

--- commands/currency/exchange-rate.js
+++ commands/currency/exchange-rate.js
@@ -4,15 +4,15 @@ const { freeCurrencyApiKey } = require('../../config.json');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('currency-exchange')
-		.setDescription('get exchange rate for one or more currency')
-		.addStringOption(option => option.setName('base-currency').setDescription('The base currency. EX: BRL/USD/EUR').setRequired(true))
-		.addStringOption(option => option.setName('currencies-to-compare').setDescription('Currency to compare').setRequired(true)),
+		.setName('exchange-rate')
+		.setDescription('Fetch the exchange rate from one currency to another')
+		.addStringOption(option => option.setName('from-currency').setDescription('The base currency. EX: BRL').setRequired(true))
+		.addStringOption(option => option.setName('to-currency').setDescription('Currency to compare. EX: USD').setRequired(true)),
 
 	async execute(interaction) {
 		try {
-			const baseCurrency = interaction.options.getString('base-currency')
-			const compareTo = interaction.options.getString('currencies-to-compare')
+			const baseCurrency = interaction.options.getString('from-currency').toUpperCase()
+			const compareTo = interaction.options.getString('to-currency').toUpperCase()
 			const response = await axios.get(`https://api.freecurrencyapi.com/v1/latest?apikey=${freeCurrencyApiKey}&base_currency=${baseCurrency}&currencies=${compareTo}`);
 
 			if (response.status === 200) {
