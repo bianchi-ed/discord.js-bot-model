f1e439d6548d36030a410ae1bf8e4cd44a8f49ed
chromeosenjoyer <134458207+chromeosenjoyer@users.noreply.github.com>
2023-05-26 05:59:26 -0300

    created kick.js slashcommand
    - add a new slashcommand to kick users

---
+++ commands/moderation/kick.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('kick')
+		.setDescription('Kick a member.')
+		.addUserOption(option => option.setName('target-user').setDescription('Target user'))
+		.setDefaultMemberPermissions(PermissionFlagsBits.KickMembers),
+	async execute(interaction) {
+		//Fetch user information
+		const user = interaction.options.getMember('target-user');
+		
+		//kick target member
+		await user.kick().catch(err => {
+			interaction.reply({content: "There was an error while running the command", ephemeral: true})
+		})
+
+		await interaction.reply(`The user ${user} was banned from this channel.`)
+	},
+};
